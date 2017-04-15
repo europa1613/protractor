d9ce617740c4e7cf21dfaec38fa608bbca552738
Arvind <europa1613@users.noreply.github.com>
2017-04-15 15:16:24 -0400

    remove node_modules

--- 01-protractortestorg/todo.spec.js
+++ 01-protractortestorg/todo.spec.js
@@ -2,7 +2,7 @@ describe('angularjs homepage todo list', function() {
     it('should add a todo', function() {
         browser.get('https://angularjs.org');
 
-        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        /*element(by.model('todoList.todoText')).sendKeys('write first protractor test');
         element(by.css('[value="add"]')).click();
 
         var todoList = element.all(by.repeater('todo in todoList.todos'));
@@ -12,6 +12,9 @@ describe('angularjs homepage todo list', function() {
         // You wrote your first test, cross it off the list
         todoList.get(2).element(by.css('input')).click();
         var completedAmount = element.all(by.css('.done-true'));
-        expect(completedAmount.count()).toEqual(2);
+        expect(completedAmount.count()).toEqual(2);*/
+
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.get(0).getText()).toEqual('learn AngularJS');
     });
 });
